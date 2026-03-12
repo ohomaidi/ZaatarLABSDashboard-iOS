a3cdb361086324b78782cfa4120111325168246e
ohomaidi <ohomaidi@hotmail.com>
2026-03-13 01:17:58 +0400

    Initial Commit

---
+++ ZaatarLABSDashboard-iOS/ZaatarLABSDashboard_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZaatarLABSDashboard_iOSApp.swift
+//  ZaatarLABSDashboard-iOS
+//
+//  Created by Omar Al Homaidi on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct ZaatarLABSDashboard_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
